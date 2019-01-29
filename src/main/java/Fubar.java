82395f7e5f988d13436524698f79aafe81e6b3c1
Sean <sean@email.com>
2019-01-28 20:50:22 -0800

    TSK-1: Oops forgot to actually add class file.

---
+++ src/main/java/Fubar.java
@@ -0,0 +1,29 @@
+public class Fubar{
+	private int id;
+	private String name;
+	
+	public Fubar(int id, String name) {
+		this.id = id;
+		this.name = name;
+	
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	
+	}
+	
+	public int getId() {
+		return id;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	
+	}
+	
+	public String getName() {
+		return name;
+	}	
+
+}
