3aa7ebca4bc3267c27190f74ba2b2ba77d5dade5
Sean <sean@email.com>
2019-01-28 20:49:28 -0800

    TSK-2: Add Fubaz POJO.

---
+++ src/main/java/Fubaz.java
@@ -0,0 +1,39 @@
+public class Fubaz{
+	private int id;
+	private String name;
+	private String location;
+	
+	public Fubaz(int id, String name, String location) {
+		this.id = id;
+		this.name = name;
+		this.location = location;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	
+	}
+	
+	public int getId() {
+		return id;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	
+	}
+	
+	public String getName() {
+		return name;
+	}	
+	
+	public void setLocation(String location) {
+		this.location = location;
+	
+	}
+	
+	public String getLocation() {
+		return location;
+	}	
+
+}
